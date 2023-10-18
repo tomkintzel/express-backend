d25be64fd99c5f031c6a4958fb5f7521e3072924
tki <kintzel@ecommerce-werkstatt.de>
2023-10-18 13:14:17 +0200

    add mysql

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -1,3 +1,27 @@
 import express from "express";
 
+import {pool} from '../config/database'
 
+const servers = [
+    {
+        "id": 1,
+        "name": "BSW Shop"
+    },
+    {
+        "id": 2,
+        "name": "BSW ERP"
+    },
+]
+
+export async function getServers() {
+    // const [rows] = await pool.query("SELECT * FROM servers")
+    // return rows
+    return servers;
+}
+
+
+export async function getServer(id) {
+    // const [rows] = await pool.query("SELECT * FROM servers WHERE id = ?", [id])
+    // return rows
+    return servers.find(server => server.id === id)
+}
--- routes/servers.js
+++ routes/servers.js
@@ -1,26 +1,30 @@
 import express from "express";
 
+import { getServers, getServer } from '../controllers/serverController.js'
+
 export const serverRouter = express.Router()
 
 serverRouter.get('/', async (req, res) => {
-	console.log('ich bin server lan')
-	res.status(200).send('ich bnin auch noch hier')
+    const servers = await getServers();
+    res.status(200).send(servers)
 })
 
-serverRouter.post('/', async(req,res) => {
-	const body = req.body
+serverRouter.post('/', async (req,res) => {
+    const body = req.body
+    if(!body) {
+        res.status(404).send('kein body uebergeben')
+    }
 
-	if(!body) {
-		res.status(404).send('kein body uebergeben')
-	}
-
-	res.status(201).send('server wurde angelegt')
+    res.status(201).send('server wurde angelegt')
 })
 
 serverRouter.get('/:id', async (req, res) => {
-	console.log('ich bin ein einzelner Server')
-	const id = await req.params.id
-	res.status(200).send(`{"id": "${id}"}`)
+    const id = +req.params.id
+    const server = await getServer(id)
+    if(!server) {
+        res.status(404).send('not found')		
+    }
+    res.status(200).send(server)
 })
 
 
